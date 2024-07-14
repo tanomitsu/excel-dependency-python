8a550006fe900f8418f8f1088038c9a6d0af5235
tanomitsu2002 <64388900+tanomitsu@users.noreply.github.com>
2024-07-14 16:21:13 +0900

    refac

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from openpyxl import load_workbook, Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 import re
-from typing import Set, List, TypeVar
+from typing import Set, List, TypeVar, Optional
 import networkx as nx
 import matplotlib.pyplot as plt
 from pyvis.network import Network
@@ -17,10 +17,8 @@ ADDRESS_PATTERN = re.compile(r"\b[A-Z]+[1-9][0-9]*(?::[A-Z]+[1-9][0-9]*)?\b")
 def extract_cells_from_formula(formula) -> List[str]:
     addresses = []
     for match in ADDRESS_PATTERN.findall(formula):
-        print(f"match: {match}")
         if ":" in match:
             # 範囲参照
-            print(f"range match: {match}")
             addresses.extend([cell for pair in rows_from_range(match) for cell in pair])
         else:
             addresses.append(match)
@@ -28,22 +26,40 @@ def extract_cells_from_formula(formula) -> List[str]:
     return list(set(addresses))
 
 
+# 1つのセルを表すクラス
+class Cell:
+    def __init__(self, sheet_name: str, address: str, value: Optional[float]):
+        self.__sheet_name = sheet_name
+        self.__address = address
+        self.__value = value
+
+    def sheet_name(self) -> str:
+        return self.__sheet_name
+
+    def address(self) -> str:
+        return self.__address
+
+    def value(self) -> Optional[float]:
+        return self.__value
+
+
 # セルの依存関係を表すクラス
 class DependencyTree:
-    def __init__(self, sheet_name: str, address: str):
-        self.sheet_name: str = sheet_name
-        self.address: str = address
-        self._dependencies: Set[DependencyTree] = set()
-        self.value = None
+    def __init__(self, cell: "Cell"):
+        self.__cell = cell
+        self.__dependencies: Set[DependencyTree] = set()
 
-    def add_dependency(self, cell: "DependencyTree") -> None:
-        self._dependencies.add(cell)
+    def add_dependency(self, parent: "DependencyTree") -> None:
+        self.__dependencies.add(parent)
 
-    def add_dependencies(self, cells: Set["DependencyTree"]) -> None:
-        self._dependencies.update(cells)
+    def add_dependencies(self, parents: Set["DependencyTree"]) -> None:
+        self.__dependencies.update(parents)
+
+    def cell(self) -> "Cell":
+        return self.__cell
 
     def dependencies(self) -> Set["DependencyTree"]:
-        return self._dependencies
+        return self.__dependencies
 
 
 # 配列からuniqueな値のみ取り出す関数
@@ -55,45 +71,42 @@ def unique_elements_preserve_order(input_list: List[T]) -> List[T]:
 def get_value_or_function(workbook: "Workbook", root_cell_address: str) -> (str, str):
     sheet: Worksheet = workbook.active
     cell = sheet[root_cell_address]
+
     if cell.data_type == "f":
+        root_cell = Cell(sheet_name="temp", address=root_cell_address, value=None)
         # Cell is function cell
         formula = cell.value
         parent_addresses = extract_cells_from_formula(formula)
         parent_addresses = unique_elements_preserve_order(parent_addresses)
-        cur = DependencyTree(sheet.title, root_cell_address)
+        cur = DependencyTree(root_cell)
         for parent_address in parent_addresses:
             parent = get_value_or_function(workbook, parent_address)
             cur.add_dependency(parent)
         return cur
-    # Cell is number cell
-    return DependencyTree(sheet.title, root_cell_address)
-
-
-# Dependency Treeを走査して
-def dfs(cur: "DependencyTree", n: int = 0):
-    print(" " * n + cur.address)
-    if n == 0:
-        for item in cur.dependencies():
-            print(item.address)
-    for dependency in cur.dependencies():
-        dfs(dependency, n + 1)
+    else:
+        # Cell is number cell
+        root_cell = Cell(sheet_name="temp", address=root_cell_address, value=cell.value)
+        return DependencyTree(root_cell)
 
 
 def add_dependency_to_graph(node: "DependencyTree", G):
-    edges = [(parent.address, node.address) for parent in node.dependencies()]
+    edges = [(parent.cell().address(), node.cell().address()) for parent in node.dependencies()]
     G.add_edges_from(edges)
     for parent in node.dependencies():
         add_dependency_to_graph(parent, G)
 
 
-def plot_dependency(root: "DependencyTree") -> None:
+def calc_dependency_graph(root: "DependencyTree") -> Network:
     # 有向グラフを作成
-    G = nx.DiGraph()
-    add_dependency_to_graph(root, G)
-    # nx.draw(G, with_labels=True)
-    # plt.show()
+    g = nx.DiGraph()
+    add_dependency_to_graph(root, g)
     net = Network(directed=True)
-    net.from_nx(G)
+    net.from_nx(g)
+    return net
+
+
+def plot_dependency(root: "DependencyTree") -> None:
+    net = calc_dependency_graph(root)
     net.show("output/dependency_tree.html", notebook=False)
 
 
@@ -102,7 +115,6 @@ def main():
     root_cell = input("Target cell address:")
     workbook: Workbook = load_workbook(book_name, data_only=False)
     root = get_value_or_function(workbook, root_cell)
-    dfs(root)
     plot_dependency(root)
 
 
